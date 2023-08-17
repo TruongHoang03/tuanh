2b117c1bd6bf43b1eee8bc30cd4ae75527a65c53
Trương Hoàng <142279911+TruongHoang03@users.noreply.github.com>
2023-08-17 22:24:44 +0700

    Add files via upload
    creat

---
+++ config.js
@@ -0,0 +1,8 @@
+const nameGirl = 'Tú Anh';
+const giftUrl = 'https://www.facebook.com/profile.php?id=100079705296222';
+const eventName = 'Chúc em luôn vui vẻ';
+const titleCard = 'Tặng người ấy';
+const contentCard = 'Chúc em luôn tràn ngập niềm vui và những nụ cười. Mong điều đẹp nhất sẽ đến với em trong hôm nay và cả những ngày sau';
+const giftImage = 'hot-girl.jpg';
+const base64 = '';
+const giftImageBase64 = "data:image/jpg;base64, " + base64;
